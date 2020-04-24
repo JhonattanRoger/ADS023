5ae63f98acbc735f5c64ed3481ca814ec7607b9e
JhonattanRoger <jhonattanroger@live.com>
2020-04-24 10:22:41 -0300

    Criando o projeto Atividade05

---
+++ Atividade05/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './screens/Login';
+import Lista from './screens/Lista';
+import Novo from './screens/Novo';
+
+export default function App() {
+  return (
+    <View style={styles.container} Novo>            
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
